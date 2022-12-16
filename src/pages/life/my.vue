8e7421e2eeea3cafc00b9e27cc887efa734729d5
tianRenYuan <1051765496@qq.com>
2022-12-16 14:43:39 +0800

    try

--- src/pages/life/my.vue
+++ src/pages/life/my.vue
@@ -1,11 +1,12 @@
 <template>
 	<view class="margin-top padding flex">
 		<image src="../../../static/iamge/tou.png" style="width: 50px;height: 50px;" mode="aspectFill"></image>
-	<!-- <view class="round cu-btn bg-black margin-left margin-top">会员中心</view> -->
-     <view class="flex">
-		 <view>183***6095</view>
+
+     <view class="">
+		 <view class="margin-left margin-top">183***6095</view>
+		 <view class="round cu-btn bg-black margin-left margin-top">会员中心</view>
 	 </view>
-	 
+	 	
 	</view>
 </template>
 
